651aa452bffb78c588d70ddd23a6f8579d59a5c0
Jahanvi-work <117625462+Jahanvi-work@users.noreply.github.com>
2023-05-24 15:06:39 +0530

    live config

--- include/Config.php
+++ include/Config.php
@@ -2,8 +2,8 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Test@12345');
+define('DB_USERNAME', 'leocamq9_learnIndiaUser');
+define('DB_PASSWORD', 'in56!@dia!@#');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'learnindia');
+define('DB_NAME', 'leocamq9_learnIndia');
 ?>
